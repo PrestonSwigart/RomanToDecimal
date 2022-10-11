8ca7a531e319b4ce515ed4a640c2d5d253081d82
Preston Swigart <prestonlswigart@gmail.com>
2022-10-11 09:05:18 -0400

    10.11.22 9:05

---
+++ src/RomanToDecimalTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+public class RomanToDecimalTest {
+
+    @org.junit.Test
+    public void romanToDecimal() {
+        assertEquals(RomanToDecimal.romanToDecimal("iv"), 4);
+
+    }
+}
